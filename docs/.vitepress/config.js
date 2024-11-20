1cd7ef21334eac0b80315701951455f1e79013c9
johonstone <junhua.zhang1229@gmail.com>
2024-11-20 21:47:09 +0800

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+// .vitepress/config.js
+export default {
+    // 站点级选项
+    title: 'VitePress',
+    base: '/Myblog/',
+    description: 'Just playing around.',
+  
+    themeConfig: {
+      // 主题级选项
+    }
+  } 
